440463fad9b8ef6677162c33289861934655c308
marekcieslar <marek.cieslar@gmail.com>
2017-08-04 21:30:15 +0200

    placed logo in menu bar

--- header.php
+++ header.php
@@ -7,6 +7,15 @@
     <?php wp_head(); ?>
 </head>
 <body>
-<header>
-header content
+<header class="page-header">
+    <div class="container"> 
+        <a href="<?php echo esc_url( home_url( '/') ); ?>" title="EMfotografia strona główna, main page"><object class="header-logo" data="<?php echo get_template_directory_uri(); ?>/logo/logoEM-web.svg" type="image/svg+xml"></object></a>
+        <nav>
+            <ul>
+                <li>O nas</li>
+                <li>Galeria</li>
+                <li>Kontakt</li>
+            </ul>
+        </nav>
+    </div>
 </header>
