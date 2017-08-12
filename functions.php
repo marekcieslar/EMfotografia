e939a3adbd8f0a3f2f13ee09560dcd251fca0db1
marekcieslar <marek.cieslar@gmail.com>
2017-08-12 07:46:58 +0200

    added custom gallelry view

--- functions.php
+++ functions.php
@@ -242,4 +242,58 @@ add_action( 'customize_register', 'emfotografia_customize_register' );
  * customize gallery
  */
 
-// TODO
+add_filter('post_gallery', 'my_post_gallery', 10, 2);
+function my_post_gallery($output, $attr) {
+    global $post;
+
+    if (isset($attr['orderby'])) {
+        $attr['orderby'] = sanitize_sql_orderby($attr['orderby']);
+        if (!$attr['orderby'])
+            unset($attr['orderby']);
+    }
+
+    extract(shortcode_atts(array(
+        'order' => 'ASC',
+        'orderby' => 'menu_order ID',
+        'id' => $post->ID,
+        'itemtag' => 'dl',
+        'icontag' => 'dt',
+        'captiontag' => 'dd',
+        'columns' => 3,
+        'size' => 'thumbnail',
+        'include' => '',
+        'exclude' => ''
+    ), $attr)); 
+    $id = intval($id);
+
+    if ('RAND' == $order) $orderby = 'none';
+
+    if (!empty($include)) {
+        $include = preg_replace('/[^0-9,]+/', '', $include);
+        $_attachments = get_posts(array('include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'ord    er' => $order, 'orderby' => $orderby));
+        $attachments = array();
+        foreach ($_attachments as $key => $val) {
+            $attachments[$val->ID] = $_attachments[$key];
+        }
+    }
+
+    if (empty($attachments)) return '';
+
+    $output = "<div class=\"gallery-container\">\n";
+  
+    foreach ($attachments as $id => $attachment) {
+
+        $img = wp_get_attachment_image_src($id, 'thumbnail');
+        $linkToFullImg = wp_get_attachment_image_src($id, 'full');
+        $output .= "<div class='gallery-thumb'>\n";
+        $output .= "<a href=\"{$linkToFullImg[0]}\">";
+        $output .= "<img src=\"{$img[0]}\" alt=\"\" />\n";
+        $output .= "</a>";
+        $output .= "</div>\n";
+    }
+
+    $output .= "</div>\n";
+
+    return $output;
+} 
+?>
