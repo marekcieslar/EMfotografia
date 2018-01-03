1064bef7b206c2e98fd76de7f7298947c01be39e
marekcieslar <marek.cieslar@gmail.com>
2018-01-03 08:05:08 +0100

    template name

--- front-page.php
+++ front-page.php
@@ -1,3 +1,13 @@
+<?php 
+/**
+ * Template Name: Full Width Page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */ 
+?>
+
 <?php get_header(); ?>
 
 <main>
