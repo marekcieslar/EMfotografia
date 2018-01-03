6ad9cf835c5964736d181bd4e33f5e3337ae30bf
marekcieslar <marek.cieslar@gmail.com>
2018-01-03 08:08:19 +0100

    name

--- fullscreen-page.php
+++ fullscreen-page.php
@@ -21,7 +21,7 @@
 
     <section id='content'>
         <div class="container">
-            <h2>Hello world!</h2>
+            <h2>Full Screen Page!</h2>
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
             <h2><?php the_title(); ?></h2>
             <?php the_content(); ?>
